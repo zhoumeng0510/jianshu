8847f52e8657840eb531557a35a7677848dece70
zhoumeng0510 <1058166619@qq.com>
2017-06-28 17:50:47 +0800

    update

--- src/main/java/jianshu/datalab/xin/servlet/UserAction.java
+++ src/main/java/jianshu/datalab/xin/servlet/UserAction.java
@@ -1,5 +1,6 @@
 package jianshu.datalab.xin.servlet;
 
+import com.alibaba.fastjson.JSON;
 import jianshu.datalab.xin.util.Db;
 import jianshu.datalab.xin.util.Error;
 import org.jasypt.util.password.StrongPasswordEncryptor;
@@ -10,9 +11,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by zhoumeng on
@@ -30,15 +34,26 @@ public class UserAction extends HttpServlet{
             return;
         }
 
-        Error.showError(req,resp);
+        if ("isNickExisted".equals(action)) {
+            isNickExisted(req, resp);
+            return;
+        }
+
+        Error.showError(req, resp);
     }
 
-    private void signUp(HttpServletRequest req, HttpServletResponse resp)throws ServletException,IOException {
-        String nick =req.getParameter("nick").trim();
+    private void signUp(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String nick = req.getParameter("nick").trim();
         String mobile = req.getParameter("mobile").trim();
 
-        // TODO: 2017.6.27 isNickExisted
-        // TODO: 2017.6.27 isMobileExisted
+        // TODO: 6/27/17 isNickExisted
+        if (isNickExisted(req, resp)) {
+            req.setAttribute("message", "...");
+            req.getRequestDispatcher("sign_up.jsp").forward(req, resp);
+            return;
+        }
+
+        // TODO: 6/27/17 isMobileExisted
 
         StrongPasswordEncryptor encryptor = new StrongPasswordEncryptor();
         String password = encryptor.encryptPassword(req.getParameter("password"));
@@ -46,32 +61,51 @@ public class UserAction extends HttpServlet{
         Connection connection = Db.getConnection();
         PreparedStatement preparedStatement = null;
 
-        String sql = "INSERT INTO db_jianshu.user(nick, mobile, password,lastIp) VALUE(?,?,?,?)";
+        String sql = "INSERT INTO db_jianshu.user(nick, mobile, password, lastIp) VALUE(?, ?, ?, ?)";
 
         try {
-            if (connection !=null) {
+            if (connection != null) {
                 preparedStatement = connection.prepareStatement(sql);
             } else {
-                Error.showError(req,resp);
+                Error.showError(req, resp);
                 return;
             }
-            preparedStatement.setString(1,nick);
-            preparedStatement.setString(2,mobile);
-            preparedStatement.setString(3,password);
-            preparedStatement.setString(4,req.getRemoteAddr());
+            preparedStatement.setString(1, nick);
+            preparedStatement.setString(2, mobile);
+            preparedStatement.setString(3, password);
+            preparedStatement.setString(4, req.getRemoteAddr());
 
             preparedStatement.executeUpdate();
 
             resp.sendRedirect("/");
         } catch (SQLException e) {
             e.printStackTrace();
-        }finally {
-            Db.close(null,preparedStatement,connection);
+        } finally {
+            Db.close(null, preparedStatement, connection);
         }
+    }
+
+    private boolean isNickExisted(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String nick = req.getParameter("nick");
+
+        // TODO: 6/28/17 SELECT * FROM db_jianshu.user WHERE nick = ?;
+
+        resp.setContentType("application/json");
+        Writer writer = resp.getWriter();
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("isNickExisted", (Math.random() > .5) ? true : false);
+
+        String json = JSON.toJSONString(map, true);
+
+        System.out.println(json);
+
+        writer.write(json);
+        return true;
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doPost(req,resp);
+        doPost(req, resp);
     }
 }
